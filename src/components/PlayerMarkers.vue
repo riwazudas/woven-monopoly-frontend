9c9083576beefa90b945be3de193dc2447685ee2
riwaz <riwazudas@gmail.com>
2026-03-23 11:36:55 +1100

    added player markers and color coding for tiles

--- src/components/PlayerMarkers.vue
+++ src/components/PlayerMarkers.vue
@@ -15,11 +15,23 @@ const props = defineProps({
 const playersOnTile = computed(() => {
   return props.players.filter((player) => Number(player.position) === props.tileIndex)
 })
+
+const markerColor = (name = '') => {
+  const palette = ['#0f766e', '#b45309', '#0369a1', '#a21caf']
+  const hash = [...name].reduce((sum, char) => sum + char.charCodeAt(0), 0)
+  return palette[hash % palette.length]
+}
 </script>
 
 <template>
   <div v-if="playersOnTile.length" class="marker-row">
-    <span v-for="player in playersOnTile" :key="player.id" class="marker" :title="player.name">
+    <span
+      v-for="player in playersOnTile"
+      :key="player.id"
+      class="marker"
+      :title="player.name"
+      :style="{ backgroundColor: markerColor(player.name) }"
+    >
       {{ player.name.slice(0, 1).toUpperCase() }}
     </span>
   </div>
